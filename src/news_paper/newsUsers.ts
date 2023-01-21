796d677d45c287b0aca699568a1e597728d07a20
Himanshu Shekhar <himanshu_shekhar@epam.com>
2023-01-21 14:18:18 +0100

    observable pattern added

---
+++ src/news_paper/newsUsers.ts
@@ -0,0 +1,38 @@
+import { displayElements } from "./displayElements";
+import { Observable } from "./observable";
+import { Observer } from "./observer";
+import { v4 as uuidv4 } from "uuid";
+
+export default class NewsUsers implements Observer, displayElements {
+  observable: Observable;
+  id: number;
+  constructor(obs: Observable) {
+    this.observable = obs;
+    this.id = uuidv4();
+  }
+
+  update(): void {
+    console.log(`user id : ${this.id} updated`);
+  }
+
+  display(): void {
+    console.log(`user id : ${this.id} display`);
+  }
+}
+
+// export class device_2 implements Observer, displayElements {
+//   observable: Observable;
+//   id: number;
+//   constructor(obs: Observable) {
+//     this.observable = obs;
+//     this.id = uuidv4();
+//   }
+
+//   update(): void {
+//     console.log("display_1 update called");
+//   }
+
+//   display(): void {
+//     console.log("update_1 display called");
+//   }
+// }
