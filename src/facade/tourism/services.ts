3f2c3f83df4214e8e292b5a5b05ceca97a48c49b
Himanshu Shekhar <himanshu_shekhar@epam.com>
2023-01-29 19:16:41 +0100

    add the facade

---
+++ src/facade/tourism/services.ts
@@ -0,0 +1,63 @@
+export class Logistic {
+  constructor() {
+    console.log("Welcome to the logistic service");
+  }
+
+  driveToAirport() {
+    console.log("Drive from home to airport");
+  }
+
+  driveToHotel() {
+    console.log("Drive from airport to hotel");
+  }
+
+  driveToHome() {
+    console.log("Drive from airport to hotel");
+  }
+}
+
+export class Hotel {
+  constructor() {
+    console.log("Welcome to the hotel");
+  }
+
+  economy() {
+    console.log("Economy service Hotel");
+  }
+
+  premiumEconomy() {
+    console.log("Premimum Economy Hotel");
+  }
+
+  premium() {
+    console.log("Premimum Hotel");
+  }
+}
+
+export class Flight {
+  constructor() {
+    console.log("Welcome to the Flight");
+  }
+
+  economy() {
+    console.log("Economy service Flight");
+  }
+
+  premiumEconomy() {
+    console.log("Premimum Economy Flight");
+  }
+
+  premium() {
+    console.log("Premimum Flight");
+  }
+}
+
+export class Escort {
+  constructor() {
+    console.log("Welcom to escort service");
+  }
+
+  service() {
+    console.log("Escort will help you to your dream destination");
+  }
+}
